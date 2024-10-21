10f380a9f4096c92b0c8670e27ff774d85497a2b
Mohin Rana <ebrahimit49@gmail.com>
2024-10-22 00:15:29 +0600

    Added home features section

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,10 @@
 import PrimaryButton from '@/components/buttons/PrimaryButton';
-import { Phone } from 'lucide-react';
+import FeaturesSection from '@/components/home/FeaturesSection';
+import { ArrowRight, Phone } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import { FaAngleRight } from 'react-icons/fa';
 import { MdOutlineLockPerson } from 'react-icons/md';
 import { VscWorkspaceTrusted } from 'react-icons/vsc';
 
@@ -11,14 +13,14 @@ const HomePage = () => {
         <>
             <header className="">
                 <div className="border-b border-gray-800 bg-customPurple py-4">
-                    <div className="container flex items-center justify-between gap-4">
+                    <div className="container flex items-center justify-between gap-4 px-4 xl:px-0">
                         <Link
                             href={'/'}
                             className="bg-gradient-to-r from-[#fb5343] to-[#059dff] bg-clip-text text-[40px] font-[900] text-transparent"
                         >
                             Bank
                         </Link>
-                        <ul className="flex items-center justify-center gap-3">
+                        <ul className="hidden items-center justify-center gap-3 md:flex">
                             <li>
                                 <Link
                                     href="#"
@@ -90,7 +92,7 @@ const HomePage = () => {
                                         </p>
                                     </div>
                                     <div>
-                                        <h1 className="text-center text-[60px] font-bold leading-[70px] text-white">
+                                        <h1 className="text-center text-[40px] font-bold leading-[46px] text-white lg:text-[60px] lg:leading-[70px]">
                                             Manage Your{' '}
                                             <span className="bg-gradient-to-r from-[#fb5343] to-[#059dff] bg-clip-text text-transparent">
                                                 Finance
@@ -99,7 +101,7 @@ const HomePage = () => {
                                         </h1>
                                     </div>
                                     <div>
-                                        <p className="mx-auto w-1/2 text-center text-gray-400">
+                                        <p className="mx-auto text-center text-gray-400 md:w-1/2">
                                             Lorem ipsum dolor sit amet
                                             consectetur adipiscing elit Ut et
                                             mass mi. Aliquam in hendrerit urna.
@@ -122,7 +124,7 @@ const HomePage = () => {
                     </div>
                 </section>
 
-                <section className="bg-customPurple py-10">
+                <section className="bg-customPurple pb-16 pt-10">
                     <div className="contaienr">
                         <div className="flex justify-center">
                             <p className="rounded bg-gradient-to-r from-[#674ad91c] to-[#fb504128] px-4 py-1">
@@ -139,7 +141,7 @@ const HomePage = () => {
                             elit. Modi, inventore!
                         </p>
                     </div>
-                    <div className="container mt-6 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:gap-7">
+                    <div className="container mt-6 grid grid-cols-1 gap-4 px-4 md:grid-cols-2 lg:grid-cols-3 xl:gap-7 xl:px-0">
                         <div className="rounded-lg bg-[#0c2a53] p-[1px]">
                             <div className="flex flex-col gap-6 rounded-lg bg-customPurple px-5 py-8">
                                 <div className="mx-auto flex h-[70px] w-[70px] items-center justify-center rounded-full bg-[#0b3c6f] ring-8 ring-[#0c2a53]">
@@ -193,7 +195,69 @@ const HomePage = () => {
                         </div>
                     </div>
                 </section>
+
+                <FeaturesSection />
+
+                {/* Best Online Bank */}
+                <section className="bg-[#1c1636] pb-16 pt-10">
+                    <div className="container border-t border-gray-600 px-4 pt-16 xl:px-0">
+                        <div className="grid grid-cols-1 gap-6 rounded-xl lg:grid-cols-2">
+                            <div className="flex items-center">
+                                <div className="flex flex-col gap-5">
+                                    <div className="flex">
+                                        <p className="rounded bg-gradient-to-r from-[#674ad91c] to-[#fb504128] px-4 py-1">
+                                            <span className="bg-gradient-to-r from-[#fb5343] to-[#c70cce] bg-clip-text font-semibold text-transparent">
+                                                Secure Transaction
+                                            </span>
+                                        </p>
+                                    </div>
+                                    <h2 className="text-[46px] font-bold leading-[50px] text-white">
+                                        Take Extra-Secure Transactions
+                                    </h2>
+                                    <p className="text-gray-500">
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing elit. Modi, inventore! Lorem
+                                        ipsum dolor sit amet consectetur
+                                        adipisicing elit. Modi, inventore!
+                                    </p>
+                                    <div>
+                                        <button
+                                            className="flex items-center gap-2 rounded-3xl px-8 py-3 text-white"
+                                            style={{
+                                                backgroundImage:
+                                                    'linear-gradient(90deg, rgb(251, 81, 65), rgb(103, 74, 217), rgb(103, 74, 217))',
+                                            }}
+                                        >
+                                            Know more <FaAngleRight />
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div
+                                className="flex items-center justify-center rounded-lg"
+                                style={{
+                                    background: `linear-gradient(90deg, rgba(101, 73, 213, 0.15) 0%, rgba(251, 83, 67, 0.15) 50.5208313465%, rgba(5, 157, 255, 0.15) 100%)`,
+                                }}
+                            >
+                                <Image
+                                    src={'/image/features-thumb-01.png'}
+                                    width={400}
+                                    height={400}
+                                    alt="image"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </main>
+            <footer className="bg-[#0e0a24] py-10">
+                <div className="container">
+                    <p className="text-center text-gray-600">
+                        Copyright © 2023{' '}
+                        <span className="text-gray-400">world bank</span>
+                    </p>
+                </div>
+            </footer>
         </>
     );
 };
